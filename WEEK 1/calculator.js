d9afdc63c77df39af473d543bc2c1ab2874bb4e4
Muna <munachi3127@gmail.com>
2026-08-14 09:13:00 -0400

    Initial commit

---
+++ WEEK 1/calculator.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+
+    return a + b;
+
+}
+
+
+
+function subtract(a, b) {
+
+    return a - b;
+
+}
+
+
+
+function multiply(a, b) {
+
+    return a * b;
+
+}
+
+
+
+function divide(a, b) {
+
+    return a / b;
+
+}
+
+
+
+console.log("Addition:", add(20, 10));
+
+console.log("Subtraction:", subtract(20, 10));
+
+console.log("Multiplication:", multiply(20, 10));
+
+console.log("Division:", divide(20, 10));
