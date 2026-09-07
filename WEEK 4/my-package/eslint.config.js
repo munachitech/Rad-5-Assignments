81851402ca5d16ff86497b9d503cdbed3fef1ebd
Muna <munachi3127@gmail.com>
2026-09-07 03:24:24 -0400

    Add W4 my-package

---
+++ WEEK 4/my-package/eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      eqeqeq: 'error',
+    },
+  },
+];
